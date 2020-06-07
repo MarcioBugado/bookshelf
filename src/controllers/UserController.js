e9ba72afbd79924489f11eb8cf83c179111d0a9d
marciobugado <you@example.com>
2020-06-07 19:05:27 -0300

    Incluir e listar Users

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,5 +5,24 @@ module.exports = {
         const results = await knex('aluno')
         
         return res.json(results)
+    }, 
+    async create(req, res, next){
+
+        try{
+            const {nome, sexo, idade, valor_pago_curso, cpf, endereco, ativo_sn} = req.body;
+            await knex('aluno').insert({
+                nome, 
+                sexo, 
+                idade, 
+                valor_pago_curso, 
+                cpf, 
+                endereco, 
+                ativo_sn
+            })
+            return res.status(201).json({nome})
+        }catch(error){
+            next(error)
+        }      
+       
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,7 @@ const UserController = require('./controllers/UserController')
 
 
 routes.get('/users', UserController.index)
+routes.post('/create', UserController.create)
+
 
 module.exports = routes;
