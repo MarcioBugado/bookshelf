212dec7b0085f952b134cef887de17eb6f292b1a
marciobugado <you@example.com>
2020-06-07 17:11:56 -0300

    Conexão DB e Rotas

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      host:'localhost',
+      database: 'universidade_u', 
+      user: 'root', 
+      password: 'jacklondon3200'
+    }
+  }
+
+};
